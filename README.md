27a711c98aa8a89b82742490ea0bff0f28d4763e
tokuhirom <tokuhirom@gmail.com>
2013-05-10 14:21:30 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Foo - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Foo;
+
+# DESCRIPTION
+
+Acme::Foo is ...
+
+# LICENSE
+
+Copyright (C) tokuhirom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tokuhirom <tokuhirom@gmail.com>
---
+++ lib/Acme/Foo.pm
@@ -0,0 +1,39 @@
+package Acme::Foo;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Foo - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Foo;
+
+=head1 DESCRIPTION
+
+Acme::Foo is ...
+
+=head1 LICENSE
+
+Copyright (C) tokuhirom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokuhirom E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
